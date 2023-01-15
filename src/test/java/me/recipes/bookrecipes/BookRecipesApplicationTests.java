5730af109448d44049c75b4023fcce7460f9c527
OlgaJav22 <olga.ko4erga@yandex.ru>
2023-01-16 01:24:10 +0300

    Готово. Хотелось бы проверить

---
+++ src/test/java/me/recipes/bookrecipes/BookRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.recipes.bookrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
